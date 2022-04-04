100d78bff1b34d301fc2e55b9a872d801a1edef3
Muratcan Akcay <muratcanakcay@gmail.com>
2022-04-04 04:04:31 +0200

    add new random dithering algorithm

--- Lab02 - Dithering and Color Quantization/Lab02 - Dithering and Color Quantization/Dithering.cs
+++ Lab02 - Dithering and Color Quantization/Lab02 - Dithering and Color Quantization/Dithering.cs
@@ -24,7 +24,7 @@ namespace Lab02___Dithering_and_Color_Quantization
             var width = clone.PixelWidth;
             var height = clone.PixelHeight;
             var rng = new Random();
-            var intervalLength = 255f / (K - 1);
+            var intervalLength = (255f / (K - 1));
 
             try
             {
@@ -36,69 +36,162 @@ namespace Lab02___Dithering_and_Color_Quantization
                     for (var y = 0; y < height; y++)
                     {
                         var oldColor = clone.GetPixelColor(x, y);
-                        var randomThreshold = (int)Math.Round(rng.NextDouble() * intervalLength);
-                        var posR = (int)Math.Truncate(1f * oldColor.R / intervalLength) + 1;
-                        var posG = (int)Math.Truncate(1f * oldColor.G / intervalLength) + 1;
-                        var posB = (int)Math.Truncate(1f * oldColor.B / intervalLength) + 1;
-                        
+                        var oldR = oldColor.R;
+                        var oldG = oldColor.G;
+                        var oldB = oldColor.B;
 
-                        if (oldColor.R == oldColor.B && oldColor.B == oldColor.G) // grey pixel
+                        var newR = 0.0;
+                        var newG = 0.0;
+                        var newB = 0.0;
+
+                        var boundariesArrayR = new int[K - 1];
+                        var boundariesArrayG = new int[K - 1];
+                        var boundariesArrayB = new int[K - 1];
+
+                        for (var i = 0; i < K - 1; i++)
                         {
-                            int greyIntensity;
-
-                            if (oldColor.R > randomThreshold)
-                            {
-                                greyIntensity = posR == K ? 255 : Math.Clamp((int)Math.Round(posR * intervalLength), 0, 255);
-                            }
-                            else
-                            {
-                                greyIntensity = posR == 1 ? 0 : Math.Clamp((int)Math.Round(posR * intervalLength), 0, 255);
-                            }
-
-                            var newColor = Color.FromArgb(oldColor.A,
-                                greyIntensity,
-                                greyIntensity,
-                                greyIntensity);
-
-                            clone.SetPixelColor(x, y, newColor);
+                            var startTempBound = i * 255.0 / (K - 1);
+                            var endTempBound = (i + 1) * 255.0 / (K - 1);
+                            boundariesArrayR[i] = rng.Next((int)startTempBound, (int)endTempBound);
+                            boundariesArrayG[i] = rng.Next((int)startTempBound, (int)endTempBound);
+                            boundariesArrayB[i] = rng.Next((int)startTempBound, (int)endTempBound);
                         }
-                        else
+
+                        if (oldR == oldG && oldG == oldB) // if greyscale
                         {
-                            int newR;
-                            int newG;
-                            int newB;
-                            
-                            newR = oldColor.R > randomThreshold ? 
-                                newR = posR == K ? 
-                                    255 : 
-                                    Math.Clamp((int)Math.Round(posR * intervalLength), 0, 255) : 
-                                newR = posR == 1 ? 
-                                    0 : 
-                                    Math.Clamp((int)Math.Round(posR * intervalLength), 0, 255);
-                            
-                            newG = oldColor.G > randomThreshold ? 
-                                newG = posG == K ? 
-                                    255 : 
-                                    Math.Clamp((int)Math.Round(posG * intervalLength), 0, 255) : 
-                                newG = posG == 1 ? 
-                                    0 : 
-                                    Math.Clamp((int)Math.Round(posG * intervalLength), 0, 255);
-                            
-                            newB = oldColor.B > randomThreshold ? 
-                                newB = posB == K ? 
-                                    255 : 
-                                    Math.Clamp((int)Math.Round(posB * intervalLength), 0, 255) : 
-                                newB = posB == 1 ? 
-                                    0 : 
-                                    Math.Clamp((int)Math.Round(posB * intervalLength), 0, 255);
+                            for (var i = 0; i < K - 1; i++)
+                            {
+                                if (oldR >= boundariesArrayR[K - 2])
+                                {
+                                    newR = 255.0;
+                                    break;
+                                }
+                                if (oldR < boundariesArrayR[i])
+                                {
+                                    newR = 255.0 * i / (K - 1);
+                                    break;
+                                }
+                            }
 
-                            var newColor = Color.FromArgb(oldColor.A,
-                                newR,
-                                newG,
-                                newB);
-
-                            clone.SetPixelColor(x, y, newColor);
+                            clone.SetPixelColor(x, y,
+                                Color.FromArgb(oldColor.A, (int)newR, (int)newR,
+                                    (int)newR));
                         }
+                        else // if RGB
+                        {
+                            for (var i = 0; i < K - 1; i++)
+                            {
+                                if (oldR >= boundariesArrayR[K - 2])
+                                {
+                                    newR = 255.0;
+                                    break;
+                                }
+                                if (oldR < boundariesArrayR[i])
+                                {
+                                    newR = 255.0 * i / (K - 1);
+                                    break;
+                                }
+                            }
+
+                            for (var i = 0; i < K - 1; i++)
+                            {
+                                if (oldG >= boundariesArrayG[K - 2])
+                                {
+                                    newG = 255.0;
+                                    break;
+                                }
+                                if (oldG < boundariesArrayG[i])
+                                {
+                                    newG = 255.0 * i / (K - 1);
+                                    break;
+                                }
+                            }
+
+                            for (var i = 0; i < K - 1; i++)
+                            {
+                                if (oldB >= boundariesArrayB[K - 2])
+                                {
+                                    newB = 255.0;
+                                    break;
+                                }
+
+                                if (oldB < boundariesArrayB[i])
+                                {
+                                    newB = 255.0 * i / (K - 1);
+                                    break;
+                                }
+                            }
+                            
+                            clone.SetPixelColor(x, y, Color.FromArgb(oldColor.A, (int)newR, (int)newG, (int)newB));
+                        }
+
+
+                        //var oldColor = clone.GetPixelColor(x, y);
+                        //var randomThresholdR = (int)Math.Round(rng.NextDouble() * intervalLength);
+                        //var randomThresholdB = (int)Math.Round(rng.NextDouble() * intervalLength);
+                        //var randomThresholdG = (int)Math.Round(rng.NextDouble() * intervalLength);
+                        //var posR = (int)Math.Truncate(oldColor.R / intervalLength) + 1;
+                        //var posG = (int)Math.Truncate(oldColor.G / intervalLength) + 1;
+                        //var posB = (int)Math.Truncate(oldColor.B / intervalLength) + 1;
+
+
+                        //if (oldColor.R == oldColor.B && oldColor.B == oldColor.G) // grey pixel
+                        //{
+                        //    int greyIntensity;
+
+                        //    if (oldColor.R % intervalLength > randomThresholdR)
+                        //    {
+                        //        greyIntensity = posR == K ? 255 : Math.Clamp((int)Math.Round(posR * intervalLength), 0, 255);
+                        //    }
+                        //    else
+                        //    {
+                        //        greyIntensity = posR == 1 ? 0 : Math.Clamp((int)Math.Round((posR-1) * intervalLength), 0, 255);
+                        //    }
+
+                        //    var newColor = Color.FromArgb(oldColor.A,
+                        //        greyIntensity,
+                        //        greyIntensity,
+                        //        greyIntensity);
+
+                        //    clone.SetPixelColor(x, y, newColor);
+                        //}
+                        //else
+                        //{
+                        //    int newR;
+                        //    int newG;
+                        //    int newB;
+
+                        //    newR = oldColor.R > randomThresholdR ?
+                        //        newR = posR == K ?
+                        //            255 :
+                        //            Math.Clamp((int)Math.Round(posR * intervalLength), 0, 255) :
+                        //        newR = posR == 1 ?
+                        //            0 :
+                        //            Math.Clamp((int)Math.Round((posR-1) * intervalLength), 0, 255);
+
+                        //    newG = oldColor.G > randomThresholdG ?
+                        //        newG = posG == K ?
+                        //            255 :
+                        //            Math.Clamp((int)Math.Round(posG * intervalLength), 0, 255) :
+                        //        newG = posG == 1 ?
+                        //            0 :
+                        //            Math.Clamp((int)Math.Round((posG-1) * intervalLength), 0, 255);
+
+                        //    newB = oldColor.B > randomThresholdB ?
+                        //        newB = posB == K ?
+                        //            255 :
+                        //            Math.Clamp((int)Math.Round(posB * intervalLength), 0, 255) :
+                        //        newB = posB == 1 ?
+                        //            0 :
+                        //            Math.Clamp((int)Math.Round((posB-1) * intervalLength), 0, 255);
+
+                        //    var newColor = Color.FromArgb(oldColor.A,
+                        //        newR,
+                        //        newG,
+                        //        newB);
+
+                        //    clone.SetPixelColor(x, y, newColor);
+                        //}
                     }
                 }
             }
