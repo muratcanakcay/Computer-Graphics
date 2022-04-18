c638489839804b269d13213a1c2dcaf85bc16acd
Muratcan Akcay <muratcanakcay@gmail.com>
2022-04-19 00:57:24 +0200

    lab03 init

---
+++ Lab03 - Rasterization/Lab03 - Rasterization/Line.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Lab03___Rasterization
+{
+    public class Line
+    {
+        public Point P1 { get; set; }
+        public Point P2 { get; set; }
+        public int Thickness { get; set; }
+
+        public Line(Point p2, Point p1, int thickness)
+        {
+            this.P2 = p2;
+            this.P1 = p1;
+            this.Thickness = thickness;
+        }
+
+        
+
+    }
+}
---
+++ Lab03 - Rasterization/Lab03 - Rasterization/WriteableBitmapExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Lab03___Rasterization
+{
+    public static class WritableBitmapExtensions
+    {
+        public static void SetPixelColor(this WriteableBitmap wbm, int x, int y, Color color)
+        {
+            if (y < 0 || x < 0 || y > wbm.PixelHeight - 1 || x > wbm.PixelWidth - 1)
+                throw new Exception("SetPixelColor target out of bitmap bounds");
+            
+            IntPtr pBackBuffer = wbm.BackBuffer;
+            int stride = wbm.BackBufferStride;
+
+            unsafe
+            {
+                var pBuffer = (byte*)pBackBuffer.ToPointer();
+                int index = y * stride + x * 4;
+
+                pBuffer[index] = color.B;
+                pBuffer[index + 1] = color.G;
+                pBuffer[index + 2] = color.R;
+                pBuffer[index + 3] = color.A;
+            }
+
+            wbm.AddDirtyRect(new Int32Rect(x, y, 1, 1));
+        }
+
+        public static Color GetPixelColor(this WriteableBitmap wbm, int x, int y)
+        {
+            if (y < 0 || x < 0||y > wbm.PixelHeight - 1 || x > wbm.PixelWidth - 1)
+                return Color.Empty;
+
+            IntPtr pBackBuffer = wbm.BackBuffer;
+            int stride = wbm.BackBufferStride;
+
+            Color pixelColor;
+
+            unsafe
+            {
+                var pBuffer = (byte*)pBackBuffer.ToPointer();
+                int index = y * stride + x * 4;
+
+                pixelColor = Color.FromArgb(pBuffer[index + 3], 
+                                            pBuffer[index + 2], 
+                                            pBuffer[index + 1], 
+                                            pBuffer[index]);
+            }
+
+            return pixelColor;
+        }
+
+        //public static WriteableBitmap ApplyFilter(this WriteableBitmap wbm, IEffect effect)
+        //{
+        //    return effect.ApplyTo(wbm);
+        //}
+    }
+}
