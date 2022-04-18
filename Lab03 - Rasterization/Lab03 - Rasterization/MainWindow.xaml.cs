27452289af7ced3ba906805bbe9d6e2fd7a2b1d9
Muratcan Akcay <muratcanakcay@gmail.com>
2022-04-19 01:35:43 +0200

    receive two clicked points from canvas

--- Lab03 - Rasterization/Lab03 - Rasterization/MainWindow.xaml.cs
+++ Lab03 - Rasterization/Lab03 - Rasterization/MainWindow.xaml.cs
@@ -21,6 +21,11 @@ namespace Lab03___Rasterization
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool _isDrawingLine;
+        private static readonly Point NullPoint = new(-1, -1);
+        private Point _startingPoint = NullPoint;
+        private Point _endingPoint = NullPoint;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -36,7 +41,38 @@ namespace Lab03___Rasterization
             var clickPosition = e.GetPosition(TheCanvas);
             Debug.WriteLine("CLICKED!");
             Debug.WriteLine($"{clickPosition.X}, {clickPosition.Y}");
-            
+
+            if (_isDrawingLine) DrawLine(clickPosition);
+
+        }
+
+        private void DrawLine(Point clickPosition)
+        {
+            if (_startingPoint.Equals(NullPoint))
+            {
+                _startingPoint = clickPosition;
+                Debug.WriteLine($"Starting: {clickPosition.X}, {clickPosition.Y}");
+            }
+            else
+            {
+                _endingPoint = clickPosition;
+                Debug.WriteLine($"Ending: {clickPosition.X}, {clickPosition.Y}");
+                ToggleIsDrawingLine();
+            }
+
+        }
+
+        private void LineButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            ToggleIsDrawingLine();
+        }
+
+        private void ToggleIsDrawingLine()
+        {
+            //TODO toggle All "isDrawing"s off first!!
+            _isDrawingLine = !_isDrawingLine;
+            LineButton.Background = _isDrawingLine ? Brushes.LightSalmon : Brushes.LightCyan;
+            if (_isDrawingLine == false) _startingPoint = NullPoint;
         }
     }
 }
